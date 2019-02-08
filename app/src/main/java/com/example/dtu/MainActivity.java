41a143f5c70583faf81826bb1fec13b98df6c8ff
Himanshu Aggarwal <himanshua04@gmail.com>
2019-02-08 13:12:58 +0530

    dtu 2.0

--- app/src/main/java/com/example/dtu/MainActivity.java
+++ app/src/main/java/com/example/dtu/MainActivity.java
@@ -1,25 +1,31 @@
 package com.example.dtu;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AbsoluteLayout;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int [][] Path= new int[20][20];
+    int row=20;
+    int column=20;
+    int [][] Path= new int[row][column];
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0;i<row;++i){
+            for(int j=0;j<column;++j){
+                Path[i][j]=1;
+            }
+        }
         touchLisner();
     }
 
@@ -44,14 +50,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void display(int x,int y){
         RelativeLayout linearview=(RelativeLayout) findViewById(R.id.touch);
-        EditText imageView=new EditText(this);
-        imageView.setLayoutParams(new android.view.ViewGroup.LayoutParams(50,50));
-        //imageView.setImageResource(R.drawable.greyimage);
+        EditText editText=new EditText(this);
+        editText.setLayoutParams(new android.view.ViewGroup.LayoutParams(50,50));
+        //editText.setImageResource(R.drawable.greyimage);
 
 
-        imageView.setBackgroundColor(Color.YELLOW);
+        editText.setBackgroundColor(Color.YELLOW);
 
-        imageView.setTextColor(Color.BLUE);
+        editText.setTextColor(Color.BLUE);
         int p=x%50;
         x=x-p;
         p=y%50;
@@ -61,15 +67,34 @@ public class MainActivity extends AppCompatActivity {
         //call to change the matrix
         updateMatrix(x,y);
 
-        imageView.setX(x);
-        imageView.setY(y);
-        linearview.addView(imageView);
+        editText.setX(x);
+        editText.setY(y);
+        linearview.addView(editText);
 
     }
     void updateMatrix(int x,int y){
         x=x/50;
         y=y/50;
-        Path[x][y]=1;
+        Path[x][y]=0;
+
+    }
+    void find(){
+        SearchMazeBFS smbfs = new SearchMazeBFS();
+
+
+        SearchMazeBFS.Point source = new SearchMazeBFS.Point(0, 0);
+        SearchMazeBFS.Point dest = new SearchMazeBFS.Point(10, 9);
+
+        int dist = smbfs.BFS(Path, source, dest);
+        TextView t1=(TextView)findViewById(R.id.value);
+        String s="hey buddy";
+
+
+        if (dist == Integer.MAX_VALUE)
+            t1.setText(s);
+        else
+            t1.setText(s);
+
 
     }
 
